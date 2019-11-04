52b9f944d3a814baa212ad5f4ba2535fc0727fb0
suyeshtiwari23 <suyesh.tiwari@outlook.com>
2019-11-04 20:41:32 +0530

    Cleaned project

---
+++ src/components/leftMenu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function LeftMenu () {
+    return (
+        <div className="LeftNav">
+            
+        </div>
+    )
+}
--- src/helpers/constants.js
+++
@@ -1,8 +0,0 @@
-export default {
-    navigation: [
-        {name:'Skills', url: 'skills'}, 
-        {name:'Academics', url: 'Academics'}, 
-        {name: 'Companies', url: 'Companies'}, 
-        {name: 'Projects', url: 'Projects'}, 
-        {name: 'About me', url: 'aboutme'}]
-}
